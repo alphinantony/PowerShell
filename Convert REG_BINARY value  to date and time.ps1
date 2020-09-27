7bdef0a0a5a12ddd0b94fc49ca4a69c80e4c2913
Your Name <you@example.com>
2020-09-28 00:12:38 +0530

    var name mod

--- Convert REG_BINARY value  to date and time.ps1
+++ Convert REG_BINARY value  to date and time.ps1
@@ -1,6 +1,7 @@
+#converting MS Defender AV signature update date (from REG_BINARY) to readable format 
 $RegValue = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Signature Updates"
-$shutDown = $RegValue.SignaturesLastUpdated
-$Int64Value = [System.BitConverter]::ToInt64($shutDown, 0)
+$SigLastUpdt = $RegValue.SignaturesLastUpdated
+$Int64Value = [System.BitConverter]::ToInt64($SigLastUpdt, 0)
 $date = [DateTime]::FromFileTime($Int64Value)
 
 $date
