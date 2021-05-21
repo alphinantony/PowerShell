9a879ac204422f6ddb9d5edb980ea39ad51ae683
alphinantony <71936573+alphinantony@users.noreply.github.com>
2021-05-22 00:28:19 +0530

    Delete GetFreeSpace.ps1

--- GetFreeSpace.ps1
+++
@@ -1,5 +0,0 @@
-$DriveFreeSpace = Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object { $_.DeviceID -eq "D:" }  | Select-Object -Property Size, DeviceID, @{'Name' = 'FreeSpace (GB)'; Expression = { [Math]::Round($_.FreeSpace / 1GB, 2) } }
-$DriveFreeSpacePerc = "{0:p1}" -f (($DriveFreeSpace.'FreeSpace (GB)') / ($DriveFreeSpace.size / 1GB))
-if ($DriveFreeSpace.'FreeSpace (GB)' -lt 30) {
-    Write-Host $($DriveFreeSpace.'FreeSpace (GB)')GB "`n"  $DriveFreeSpacePerc
-}
